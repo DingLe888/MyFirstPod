4f3f0e46884c372379855beae660d477aa3c4f00
dingle <18911755085@163.com>
2017-10-21 16:55:34 +0800

    2

---
+++ Dog.h
@@ -0,0 +1,14 @@
+//
+//  Dog.h
+//  Pods
+//
+//  Created by 丁乐 on 2017/10/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
++(void)getDogName;
+@end
---
+++ Dog.m
@@ -0,0 +1,16 @@
+//
+//  Dog.m
+//  Pods
+//
+//  Created by 丁乐 on 2017/10/21.
+//
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
++(void)getDogName{
+    NSLog(@"my name is Tom");
+}
+@end
